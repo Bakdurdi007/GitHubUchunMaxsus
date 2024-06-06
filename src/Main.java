8acf2dd82326e51afd03b1eeabc15ace0ca1ac9f
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-06-06 15:47:43 +0500

    Bu 3-kommentim.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Assalom alaykum barchaga! ");
+        System.out.println("Sardorbek");
+        System.out.println("Sirojiddin");
+        System.out.println("Saidaxror");
+        System.out.println("Salohiddin");
+        System.out.println("Abdurauf");
+        System.out.println("Boburbek");
+        System.out.println("Bekzodbek");
+        System.out.println("Abdulaziz");
     }
 }
